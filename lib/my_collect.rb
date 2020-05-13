d2a5a55d8c74a81f6bbd94ac3bf3fb7871fe3e2b
Dustin Blanton <dustinblntn@gmail.com>
2020-05-13 23:00:04 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -9,9 +9,9 @@ def my_collect(array)
   i += 1 
   
 end
-  if i >= array.length
-    return new_collect.downcase
-  end
+  # if i >= array.length
+  #   return new_collect
+  # end
 
 new_collect
   
